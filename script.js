517a2a835c7c18f6ea1ae55e8ea765a9d9a11764
MyNa Do <myna.do@mahoyo.com>
2019-12-18 12:21:38 +0100

    Added two functions getArtist() and renderSearchResult()

--- script.js
+++ script.js
@@ -2,3 +2,30 @@
  * Music app.
  * 
  */
+
+// Get input
+
+ const getArtist = async () => {
+    const response = await fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=beyonce", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+		"x-rapidapi-key": "79e8812e73msh8da75390c6b6b51p184777jsn86eb54cfc71a"
+	}
+    });
+    if (!response.ok) {
+        throw new Error("Response was not OK.");
+    }
+   
+    return await response.json();
+}
+
+const renderSearchResults = (results) => {
+    results.data.forEach(result => {
+        console.log(result);
+    })
+};
+
+getArtist().then(renderSearchResults).catch(err => {
+    alert("Error getting artist. Error was: " + err);
+});
